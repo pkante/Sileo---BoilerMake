63029bf8c5bac9f8582678c1bc3730e91cba8522
pkante <pranavkante@pal-10-184-81-199.itap.purdue.edu>
2023-01-22 04:52:09 -0500

    failure lol

--- sileo/src/pages/Home.jsx
+++ sileo/src/pages/Home.jsx
@@ -1,17 +1,15 @@
-import React from "react";
+import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Chat from '../components/Chat'
 
 const Home = () => {
-    return (
-        <div className = 'home'>
-            <div className  = "container">
-                <Sidebar/>
-                <Chat/>
-            </div>
-        </div>
-    )
-    
+  return (
+    <div className='home'>
+      <div className="container">
+        <Sidebar/>
+        <Chat/>
+      </div>
+    </div>
+  )
 }
-
 export default Home
